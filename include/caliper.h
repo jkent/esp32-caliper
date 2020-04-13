a273220497e439c88ff055665d511c92cd98bc2f
Jeff Kent <jeff@jkent.net>
2020-04-13 02:41:00 -0500

    polling and callback systems

--- include/caliper.h
+++ include/caliper.h
@@ -1,11 +1,15 @@
 #include <esp_err.h>
 
-typedef enum caliper_mode_t {
-    CALIPER_MODE_MM,
-    CALIPER_MODE_INCHES,
-} caliper_mode_t;
+typedef enum {
+    CALIPER_UNITS_MM,
+    CALIPER_UNITS_INCHES,
+} caliper_units_t;
+
+typedef struct caliper* caliper_handle;
+
+typedef void (*caliper_cb_t)(caliper_handle handle, double value, caliper_units_t units, bool power);
 
 esp_err_t caliper_init(void);
-struct caliper* caliper_add(int clock_pin, int data_pin);
-void caliper_remove(struct caliper* caliper);
-double caliper_value(struct caliper* caliper, caliper_mode_t* mode, bool* power);
+struct caliper* caliper_add(int clock_pin, int data_pin, caliper_cb_t cb);
+void caliper_remove(caliper_handle handle);
+void caliper_poll(caliper_handle handle, double* value, caliper_units_t* units, bool* power);
--- src/caliper.c
+++ src/caliper.c
@@ -1,76 +1,173 @@
 #include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/queue.h>
 
 #include "caliper.h"
 #include "driver/gpio.h"
+#include "driver/timer.h"
 #include "esp_log.h"
 #include "sys/queue.h"
 
 const char TAG[] = "caliper";
 
 #define CALIPER_HOST SPI3_HOST
-
-SLIST_HEAD(caliper_head, caliper) caliper_head =
-    SLIST_HEAD_INITIALIZER(caliper_head);
+#define TIMER_DIVIDER (16)
+#define TIMER_SCALE (TIMER_BASE_CLK / TIMER_DIVIDER)
 
 struct caliper {
     SLIST_ENTRY(caliper) entries;
     int clock_pin;
     int data_pin;
+    caliper_cb_t cb;
     portMUX_TYPE mutex;
+    uint32_t gpio_data;
+    uint8_t gpio_bits;
+    volatile bool power;
     volatile uint32_t sample;
     volatile int64_t last_seen;
-    uint32_t isr_data;
-    uint32_t isr_bit_count;
 };
+
+SLIST_HEAD(caliper_head, caliper) caliper_head =
+    SLIST_HEAD_INITIALIZER(caliper_head);
+
+static xQueueHandle queue = NULL;
+
+
+static void IRAM_ATTR gpio_clock_isr_handler(void* arg)
+{
+    caliper_handle handle = (caliper_handle)arg;
+
+    int clock = gpio_get_level(handle->clock_pin); /* TODO: invert */
+    int data = gpio_get_level(handle->data_pin); /* TODO: invert */
+    uint64_t now = esp_timer_get_time();
+
+    portENTER_CRITICAL_ISR(&handle->mutex);
+    if (clock) {
+        handle->power = true;
+        handle->gpio_data = (data << 23) | (handle->gpio_data >> 1);
+        handle->gpio_bits += 1;
+        if (handle->gpio_bits == 24) {
+            handle->sample = handle->gpio_data;
+            xQueueSendFromISR(queue, &handle, NULL);
+        }
+
+    } else if (now - handle->last_seen > 1000) {
+        handle->gpio_bits = 0;
+    }
+    handle->last_seen = now;
+    portEXIT_CRITICAL_ISR(&handle->mutex);
+}
+
+
+static void IRAM_ATTR timer_isr_handler(void* arg)
+{
+    timer_spinlock_take(TIMER_GROUP_0);
+    caliper_handle handle;
+
+    uint64_t now = esp_timer_get_time();
+
+    SLIST_FOREACH(handle, &caliper_head, entries) {
+        portENTER_CRITICAL_ISR(&handle->mutex);
+        if (handle->power && (now - handle->last_seen >= 200000)) {
+            handle->power = false;
+            xQueueSendFromISR(queue, &handle, NULL);
+        }
+        portEXIT_CRITICAL_ISR(&handle->mutex);
+    }
+
+    timer_group_clr_intr_status_in_isr(TIMER_GROUP_0, 0);
+    timer_group_enable_alarm_in_isr(TIMER_GROUP_0, 0);
+    timer_spinlock_give(TIMER_GROUP_0);
+}
+
+
+static void caliper_task(void* arg)
+{
+    caliper_handle handle;
+
+    while (true) {
+        if (xQueueReceive(queue, &handle, portMAX_DELAY)) {
+            if (!handle->cb) {
+                continue;
+            }
+
+            portENTER_CRITICAL(&handle->mutex);
+            int sample = handle->sample;
+            bool power = handle->power;
+            portEXIT_CRITICAL(&handle->mutex);
+
+            bool sign = (sample >> 20) & 1;
+            bool units = (sample >> 23) & 1;
+            double value = sample & 0x0FFFFF;
+            if (sign) {
+                value *= -1;
+            }
+            if (units == CALIPER_UNITS_MM) {
+                value = value / 100.0l;
+            } else {
+                value = value / 10000.0l;
+            }
+
+            handle->cb(handle, value, units, power);
+        }
+    }
+}
 
 
 esp_err_t caliper_init(void)
 {
-    if (SLIST_FIRST(&caliper_head) != NULL) {
+    if (queue != NULL) {
         ESP_LOGE(TAG, "already initialized!");
         return ESP_FAIL;
     }
 
+    esp_err_t err = gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Could not install gpio isr service!");
+        return err;
+    }
+
     SLIST_INIT(&caliper_head);
-    gpio_install_isr_service(0);
+
+    queue = xQueueCreate(5, sizeof(struct caliper*));
+    xTaskCreate(caliper_task, "caliper_task", 2048, NULL, 10, NULL);
+
+    timer_config_t timer_config = {
+        .divider = TIMER_DIVIDER,
+        .counter_dir = TIMER_COUNT_UP,
+        .counter_en = TIMER_PAUSE,
+        .alarm_en = TIMER_ALARM_EN,
+        .intr_type = TIMER_INTR_LEVEL,
+        .auto_reload = true,
+#ifdef TIMER_GROUP_SUPPORTS_XTAL_CLOCK
+        .clk_src = TIMER_SRC_CLK_APB,
+#endif
+    };
+
+    timer_init(TIMER_GROUP_0, 0, &timer_config);
+    timer_set_counter_value(TIMER_GROUP_0, 0, 0ULL);
+    timer_set_alarm_value(TIMER_GROUP_0, 0, 0.1l * TIMER_SCALE);
+    timer_enable_intr(TIMER_GROUP_0, 0);
+    timer_isr_register(TIMER_GROUP_0, 0, timer_isr_handler, NULL,
+        ESP_INTR_FLAG_IRAM, NULL);
+    timer_start(TIMER_GROUP_0, 0);
 
     return ESP_OK;
 }
 
 
-static void IRAM_ATTR gpio_clock_isr_handler(void* arg)
+caliper_handle caliper_add(int clock_pin, int data_pin, caliper_cb_t cb)
 {
-    struct caliper* caliper = (struct caliper*) arg;
+    caliper_handle handle = calloc(1, sizeof(struct caliper));
+    assert(handle != NULL);
 
-    int clock = gpio_get_level(caliper->clock_pin); /* TODO: invert */
-    int data = gpio_get_level(caliper->data_pin); /* TODO: invert */
-    uint64_t now = esp_timer_get_time();
+    vPortCPUInitializeMutex(&handle->mutex);
 
-    portENTER_CRITICAL_ISR(&caliper->mutex);
-    if (clock) {
-        caliper->isr_data = (data << 23) | (caliper->isr_data >> 1);
-        caliper->isr_bit_count += 1;
-        if (caliper->isr_bit_count == 24) {
-            caliper->sample = caliper->isr_data;
-        }
-    } else if (now - caliper->last_seen > 1000) {
-        caliper->isr_bit_count = 0;
-    }
-    caliper->last_seen = now;
-    portEXIT_CRITICAL_ISR(&caliper->mutex);
-}
+    handle->clock_pin = clock_pin;
+    handle->data_pin = data_pin;
+    handle->cb = cb;
 
-
-struct caliper* caliper_add(int clock_pin, int data_pin)
-{
-    struct caliper *caliper = calloc(1, sizeof(struct caliper));
-    assert(caliper != NULL);
-
-    vPortCPUInitializeMutex(&caliper->mutex);
-
-    caliper->clock_pin = clock_pin;
-    caliper->data_pin = data_pin;
-    caliper->sample = 0;
+    handle->sample = 0;
 
     gpio_config_t config = {
         .pin_bit_mask = 1 << clock_pin | 1 << data_pin,
@@ -80,40 +177,50 @@ struct caliper* caliper_add(int clock_pin, int data_pin)
     };
     gpio_config(&config);
     gpio_set_intr_type(clock_pin, GPIO_INTR_ANYEDGE);
-    gpio_isr_handler_add(clock_pin, gpio_clock_isr_handler, caliper);
+    gpio_isr_handler_add(clock_pin, gpio_clock_isr_handler, handle);
 
-    SLIST_INSERT_HEAD(&caliper_head, caliper, entries);
-    return caliper;
+    SLIST_INSERT_HEAD(&caliper_head, handle, entries);
+
+    return handle;
 }
 
 
-void caliper_remove(struct caliper* caliper)
+void caliper_remove(caliper_handle handle)
 {
-    gpio_isr_handler_remove(caliper->clock_pin);
+    gpio_isr_handler_remove(handle->clock_pin);
 
-    SLIST_REMOVE(&caliper_head, caliper, caliper, entries);
+    SLIST_REMOVE(&caliper_head, handle, caliper, entries);
+
+    free(handle);
 }
 
-
-double caliper_value(struct caliper* caliper, caliper_mode_t* mode, bool* power)
+void caliper_poll(caliper_handle handle, double* value, caliper_units_t* units, bool* power)
 {
-    int64_t now = esp_timer_get_time();
+    portENTER_CRITICAL(&handle->mutex);
+    int sample = handle->sample;
+    bool _power = handle->power;
+    portEXIT_CRITICAL(&handle->mutex);
 
-    portENTER_CRITICAL(&caliper->mutex);
-    int sample = caliper->sample;
+    bool sign = (sample >> 20) & 1;
+    bool _units = (sample >> 23) & 1;
+    if (value) {
+        double _value = sample & 0x0FFFFF;
+        if (sign) {
+            _value *= -1;
+        }
+        if (units == CALIPER_UNITS_MM) {
+            _value = _value / 100.0l;
+        } else {
+            _value = _value / 10000.0l;
+        }
+        *value = _value;
+    }
+
+    if (units) {
+        *units = _units;
+    }
+
     if (power) {
-        *power = now - caliper->last_seen < 200000;
+        *power = _power;
     }
-    portEXIT_CRITICAL(&caliper->mutex);
-
-    int sign = (sample >> 20) & 1;
-    int _mode = (sample >> 23) & 1;
-    sample &= 0x0FFFFF;
-    if (sign) {
-        sample *= -1;
-    }
-    if (mode) {
-        *mode = _mode;
-    }
-    return _mode ? (sample * 5) / 10000.0l : sample / 100.0l;
 }
